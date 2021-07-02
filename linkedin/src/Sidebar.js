cdd1b3261b6c94f870e55c218f81311304850e58
kelvinrandu <randukelvin@gmail.com>
2021-07-02 15:55:24 +0300

    styled top sidebar

--- linkedin/src/Sidebar.js
+++ linkedin/src/Sidebar.js
@@ -6,13 +6,25 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img src="https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" />
         <Avatar className="sidebar_avatar" />
         <h2>robert kelly</h2>
         <h4> rkelly@universal.com</h4>
       </div>
       <div className="sidebar__stats">
-        <div className="sidebar__stat"></div>
+        <div className="sidebar__stat">
+          <p>who viewed youw</p>
+          <p className="sidebar__statNumber">2,545</p>
+
+        </div>
+        <div className="sidebar__stat">
+        <p>views on post</p>
+          <p className="sidebar__statNumber">2,545</p>
+            
+            </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>recent</p>
       </div>
     </div>
   );
